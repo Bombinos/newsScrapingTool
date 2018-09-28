c295c5ba2c0026e3e24b20b38d4792e82b679265
Matthew Polson <38453024+Bombinos@users.noreply.github.com>
2018-09-28 00:50:11 -0600

    newsies

---
+++ main.js
@@ -0,0 +1,107 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/newsScraper", {
+  useMongoClient: true
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+app.get("/scrape", function(req, res) {
+
+  axios.get("http://www.espn.com/").then(function(response) {
+
+    var $ = cheerio.load(response.data);
+
+	$("div.contentItem__contentWrapper").each(function(i, element) {
+
+      var result = {};
+
+      result.title = $(this).children().text();
+      result.link = $(this).parent().attr("href");
+      result.summary = $(this).children().attr("p");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+
+          return res.json(err);
+        });
+    });
+
+    res.send("Scraping Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+
+  db.Article.find({})
+    .then(function(dbArticle) {
+
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+ 
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+
+      res.json(err);
+    });
+
+});
+
+app.post("/articles/:id", function(req, res) {
+ 
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbUser) {
+
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+
+      res.json(err);
+    });
+});
+
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT);
+});
---
+++ public/app.js
@@ -0,0 +1,62 @@
+
+$.getJSON("/articles", function(data) {
+
+    for (var i = 0; i < data.length; i++) {
+
+      $("#articles").append("<div>" + "<p>" + "<strong>Title: </strong>" + data[i].title + "<br />" + "<strong>Summary: </strong>" + data[i].summary + "<br />" + "<strong>Link: </strong>" + "<a href='" + data[i].link + "'>" + "ESPN" + "</a>" + "</p>" + "<button class='saveArticle'" + "data-id='" + data[i]._id + "'>" + "Save Article</button>" + "</div>" + "<br>");
+    }
+  });
+  
+  $(document).on("click", "#run-scrape", function() {
+
+      location.href = "/scrape"
+  
+  });
+  
+  $(document).on("click", "#show-articles", function() {
+    $.getJSON("/articles", function(data) {
+
+      location.href = "/"
+      for (var i = 0; i < data.length; i++) {
+
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<strong>Title: </strong>" + data[i].title + "<br />" + "<strong>Summary: </strong>" + data[i].summary + "<br />" + "<strong>Link: </strong>" + "<a href='" + data[i].link + "'>" + "ESPN" + "</a>" + "</p>");
+      };
+    });
+  });
+  
+  $(document).on("click", ".articleSave", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+      method: "POST",
+      url: "/articleSave/" + thisId,
+      data: {
+          saved: "yes"
+      }
+    });
+  });
+
+  $(document).on("click", "#savenote", function() {
+
+    var thisId = $(this).attr("data-id");
+  
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+
+        title: $("#titleinput").val(),
+
+        body: $("#bodyinput").val()
+      }
+    });
+
+      .then(function(data) {
+
+        console.log(data);
+
+        $("#notes").empty();
+      });
+  
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
